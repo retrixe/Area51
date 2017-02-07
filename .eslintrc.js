d022928bdcca996f8655a046d9cf531c37e2f1d8
Ibrahim Ansari <ansari.ibrahim1@gmail.com>
2017-02-07 20:48:18 +0530

    Initial setup commit

---
+++ .eslintrc.js
@@ -0,0 +1,50 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "experimentalObjectRestSpread": true,
+            "jsx": true
+        },
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "indent": [
+            "error",
+            2
+        ],
+        "linebreak-style": [
+            "error",
+            "windows"
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "no-unused-vars": [
+          "error",
+          {
+            "vars": "local",
+            "args": "after-used"
+          }
+        ],
+        "no-mixed-operators": [
+          "off"
+        ],
+        "no-var": [
+            "off"
+        ]
+    }
+};
