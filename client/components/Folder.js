144a478d7ac3581f53683625e7ad8eb64f183468
Ibrahim Ansari <ansari.ibrahim1@gmail.com>
2017-04-19 17:59:43 +0530

    Add functionality to go up one directory.

--- client/components/Folder.js
+++ client/components/Folder.js
@@ -16,12 +16,13 @@ import ListCreator from "../../imports/ui/ListCreator";
 import { folderToShow } from "../../settings.json";
 /* eslint-enable no-unused-vars */
 
+// Create the class.
 export default class Folder extends React.Component<any, any, any> {
   constructor() {
     super();
 
     this.state = {
-      listItem: [
+      listItems: [
         {
           name: "Please wait, data is being fetched from the server.",
           type: "dataFetch",
@@ -32,38 +33,54 @@ export default class Folder extends React.Component<any, any, any> {
   }
 
   componentDidMount() {
-    const goToParentDirectory = {
-      name: "Go to parent directory",
-      type: "..",
-    };
+    // Fetch directory contents in async mode.
     Meteor.call("getFolderContents", folderToShow, (err, result) => {
-      this.setState({ listItem: [goToParentDirectory, ...result] });
+      // set state to the go back item and the items in the directory.
+      this.setState({ listItems: [{
+        name: "Go to parent directory",
+        type: "..",
+      }, ...result] });
     });
   }
 
   onItemClick(type: string, addition: string) {
     if (type === "folder") {
+      // Ask server to join paths.
       Meteor.call("joinPaths", this.state.currentFolder, addition, (err, result) => {
+        // In callback, set currentFolder to the joint result.
         this.setState({ currentFolder: result });
-        const goToParentDirectory = {
-          name: "Go to parent directory",
-          type: "..",
-        };
-        Meteor.call("getFolderContents", folderToShow, (error, files) => {
-          this.setState({ listItem: [goToParentDirectory, ...files] });
+        // Now get the folder contents for the result.
+        Meteor.call("getFolderContents", this.state.currentFolder, (error, files) => {
+          // set state to the go back item and the items in the directory.
+          this.setState({ listItems: [{
+            name: "Go to parent directory",
+            type: "..",
+          }, ...files] });
         });
       });
     } else if (type === "file") {
       console.log("hellooooo! wheeeee! cookies! I'm a fileeee!");
     } else {
-      console.log("hellooo! wheeee! wanna go up! cookies!!!!!");
+      // Ask server to remove the last directory to go up one directory.
+      Meteor.call("goUpOneDirectory", this.state.currentFolder, (err, result) => {
+        // In callback, set currentFolder to the joint result.
+        this.setState({ currentFolder: result });
+        // Now get the folder contents for the result.
+        Meteor.call("getFolderContents", this.state.currentFolder, (error, files) => {
+          // set state to the go back item and the items in the directory.
+          this.setState({ listItems: [{
+            name: "Go to parent directory",
+            type: "..",
+          }, ...files] });
+        });
+      });
     }
   }
 
   render() {
     return (
       <List>
-        <ListCreator list={this.state.listItem} onItemClick={(t, a) => this.onItemClick(t, a)} />
+        <ListCreator list={this.state.listItems} onItemClick={(t, a) => this.onItemClick(t, a)} />
       </List>
     );
   }
--- server/main.js
+++ server/main.js
@@ -7,23 +7,42 @@ import { Meteor } from "meteor/meteor";
 import fs from "fs";
 import path from "path";
 
+// Create the Meteor methods.
 Meteor.methods({
+  // This method enables the client to get the contents of any folder.
   getFolderContents(folder) {
+    // Get folder contents and create initial variables the loop will write to.
     const folderContents = fs.readdirSync(folder);
     const folderContentsWithTypes = [];
     let i;
+    // Define the function to get the type of a directory item.
     const getType = () => {
       if (fs.lstatSync(`${folder}/${folderContents[i]}`).isDirectory()) {
         return "folder";
       }
       return "file";
     };
+    // Start the loop.
     for (i = 0; i < folderContents.length; i += 1) {
+      // Push objects to folderContentsWithTypes.
       folderContentsWithTypes.push({ name: folderContents[i], type: getType() });
     }
+    // Return folderContentsWithTypes.
     return folderContentsWithTypes;
   },
+  // Pass it some paths and get a combination of those paths.
   joinPaths(...paths) {
-    path.join(paths);
+    return path.join(...paths);
+  },
+  goUpOneDirectory(pathy) {
+    const pathyArray = pathy.split(path.sep);
+    if (pathyArray[0] === "") {
+      pathyArray[0] = "/";
+    }
+    const newArray = [];
+    for (let x = 0; x < pathyArray.length - 1; x += 1) {
+      newArray.push(pathyArray[x]);
+    }
+    return path.join(...newArray);
   },
 });
